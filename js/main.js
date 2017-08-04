5ff97da3ae58952c677e1bb2f4faa0e3c30d7540
Alan Lu <alan48085@gmail.com>
2017-08-04 15:26:06 -0400

    add alert

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var page ="Page is still in the works";
+
+alert(page);
+//Don't have to create var can just use alert("Page is still....")
+
+function myFunction() {
+  alert("Coming soon. Please send message to alanylu@live.unc.edu")
+}
